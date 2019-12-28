ad44f0278a2b62f37881e582e3985c25d56cb428
Dhanavarsha <dhan.varsha03@gmail.com>
2019-12-27 21:40:41 -0800

    Adds initial version of flaky test detector

---
+++ bug_tracking/trello.py
@@ -0,0 +1,53 @@
+import requests
+
+class Trello:
+    
+    BASE_URL = "https://api.trello.com/1/"
+
+    def __init__(self, trello_config):
+        self.trello_config = trello_config
+
+    def make_trello_task(self, testcase_name, failures):
+        list_id = self._get_list_id()
+        standard_desc = "The flaky-test-detector observed the following failures:\n"
+        for failure in failures:
+            standard_desc += "```\n" + failure + "\n```\n"
+        querystring = { "idList":list_id,
+                        "keepFromSource":"all",
+                        "name":testcase_name + " is flaky",
+                        "desc":standard_desc}
+        self._call_trello("POST", "/cards", params=querystring)
+
+    def _get_list_id(self):
+        board_id = self._get_board_id()
+        path = "boards/{}/lists".format(board_id)
+        querystring = {"cards":"none",
+                       "card_fields":"all",
+                       "filter":"open",
+                       "fields":"all",
+                       }
+        board_lists = self._call_trello("GET", path, params=querystring)
+        for board_list in board_lists:
+            if board_list["name"] == self.trello_config["board_list"]:
+                    return board_list["id"]
+
+    def _get_board_id(self):
+        querystring = { "filter":"all",
+                        "fields":"all",
+                        "lists":"none",
+                        "memberships":"none",
+                        "organization":"false",
+                        "organization_fields":"name,displayName",
+                    }
+        boards =  self._call_trello("GET", "members/me/boards", params=querystring)
+        board_id = None
+        for board in boards:
+            if board["name"] == self.trello_config["board_name"]:
+                    return board["id"]
+
+    def _call_trello(self, method_name, path, params={}):
+        url = Trello.BASE_URL + path
+        params.update({ "key":self.trello_config["api_key"],
+                      "token":self.trello_config["access_token"],
+                     })
+        return requests.request(method_name, url, params=params).json()
---
+++ flakyTestDetector.py
@@ -0,0 +1,75 @@
+#!/usr/local/bin/python3
+import yaml
+import tempfile
+import subprocess
+import os
+import xml.etree.ElementTree as ET
+from distutils.dir_util import copy_tree
+from bug_tracking import trello 
+
+def readconfig(config_file):
+    with open(config_file) as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+        return config
+
+def clone(repo_url, dir_path):
+    try:
+        subprocess.check_output(["git", "clone", repo_url, dir_path])
+        return True
+    except subprocess.CalledProcessError as error:
+        print("An error occured: \n{}".format(error.output))
+        return False
+
+def execute_test(test_command, cwd):
+    try:
+        subprocess.check_output(test_command, shell=True, cwd=cwd)
+        return True
+    except subprocess.CalledProcessError as error:
+        print("An error occured: \n{}".format(error.output))
+        return False
+
+config = readconfig("config.yaml")
+repo_url = config["repository"]
+test_command = config["command"]
+test_report = config["testreport"]
+trello_config = config["trello"]
+number_of_invocations = int(config["numberOfInvocations"])
+
+with tempfile.TemporaryDirectory() as temp_dir:
+    clone_dir_path = temp_dir + "/cloned"
+    test_report_backup = temp_dir + "/backup"
+    clone_success = clone(repo_url, clone_dir_path)
+    if clone_success:
+        os.mkdir(test_report_backup)
+        for i in range(number_of_invocations):
+            execute_test(test_command, clone_dir_path)
+            toDirectory = test_report_backup + "/" + str(i)
+            os.mkdir(toDirectory)
+            fromDirectory = clone_dir_path + "/" + test_report
+            copy_tree(fromDirectory, toDirectory)
+
+        test_results = {}
+        for i in range(number_of_invocations):
+            test_report_dir = test_report_backup + "/" + str(i)
+            for filename in  os.listdir(test_report_dir):
+                if filename.endswith(".xml"):
+                    tree = ET.parse(test_report_dir + "/" + filename)
+                    testsuite = tree.getroot()
+                    for testCase in testsuite.findall('testcase'):
+                        testcase_name = testCase.get('name')
+                        testcase_class = testCase.get('classname')
+                        full_test_name_key = testcase_class + "." + testcase_name
+                        if full_test_name_key not in test_results:
+                            test_results[full_test_name_key] = []
+                        for child in testCase:
+                            if child.tag == "failure":
+                                failure_reason = child.get('message')
+                                if failure_reason is None:
+                                    failure_reason = child.text
+
+                                test_results[full_test_name_key].append(failure_reason)
+        for test in test_results:
+            if len(test_results[test]) != number_of_invocations and len(test_results[test]) != 0:
+                print("{} is flaky".format(test))
+                t = trello.Trello(trello_config)
+                t.make_trello_task(test, test_results[test])
